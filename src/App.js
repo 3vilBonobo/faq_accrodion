a39103cf65bb917181f083aeea860d661193025f
3vilBonobo <iboutsika@gmail.com>
2021-10-16 13:59:29 +0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Attribution from './Components/Attribution';
 import './Sass/App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Attribution />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Attribution.js
@@ -0,0 +1,21 @@
+const Attribution = () => {
+  return (
+    <div className="attribution">
+      Challenge by{' '}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href="https://twitter.com/3Bonobo" rel="noreferrer" target="_blank">
+        Irinella Boutsika
+      </a>
+      .
+    </div>
+  );
+};
+
+export default 'Attribution';
